c5505268586ba6a0c0c38bb3b578ddec27a7beb1
szyszka <szysza0x@gmail.com>
2016-04-06 16:34:07 +0200

    newNewVersionOfMapV0.0.3

--- scripts/main.js
+++ scripts/main.js
@@ -1,209 +1,226 @@
-// map with markers
-var map;
-var infowindow;
-var monuments = [
-  {
-    nazwa: 'Bazylika Mariacka',
-    typ: 'kościół',
-    adres:{
-      street: 'Reja',
-      number: 4,
-      position: {
-        lat: 54.349985,
-        lng: 18.653242
-      }
+(function () {
+  var monuments = [
+    {
+      nazwa: 'Bazylika Mariacka',
+      typ: 'kościół',
+      adres: {
+        street: 'Reja',
+        number: 4,
+        position: {
+          lat: 54.349985,
+          lng: 18.653242
+        }
+      },
+      about: 'krótki opis',
+      image: 'jakies zdjecie',
+      WHstatus: true,
+      ID: 'xxxx'
+
     },
-    about: 'krótki opis',
-    image: 'jakies zdjecie',
-    WHstatus: true,
-    ID: 'xxxx'
+    {
+      nazwa: 'Muzeum II Wojny Światowej',
+      typ: 'muzeum',
+      adres: {
+        street: 'Mickiewicza',
+        number: 4,
+        position: {
+          lat: 54.349718,
+          lng: 18.648417
+        }
+      },
+      about: 'krótki opis',
+      image: 'jakies zdjecie',
+      WHstatus: true,
+      ID: 'xxxx'
 
-  },
-  {
-    nazwa: 'Muzeum II Wojny Światowej',
-    typ: 'muzeum',
-    adres:{
-      street: 'Mickiewicza',
-      number: 4,
-      position: {
-        lat: 54.349718,
-        lng: 18.648417
-      }
     },
-    about: 'krótki opis',
-    image: 'jakies zdjecie',
-    WHstatus: true,
-    ID: 'xxxx'
+    {
+      nazwa: 'Pomnik Obrońców Wybrzeża',
+      typ: 'pomnik',
+      adres: {
+        street: 'Reja',
+        number: 4,
+        position: {
+          lat: 54.406786,
+          lng: 18.667375
+        }
+      },
+      about: 'krótki opis',
+      image: 'jakies zdjecie',
+      WHstatus: true,
+      ID: 'xxxx'
 
-  },
-  {
-    nazwa: 'Pomnik Obrońców Wybrzeża',
-    typ: 'pomnik',
-    adres:{
-      street: 'Reja',
-      number: 4,
-      position: {
-        lat: 54.406786,
-        lng: 18.667375
-      }
     },
-    about: 'krótki opis',
-    image: 'jakies zdjecie',
-    WHstatus: true,
-    ID: 'xxxx'
+    {
+      nazwa: 'Kościół Świętej Trójcy',
+      typ: 'kościół',
+      adres: {
+        street: 'Szymborskiej',
+        number: 4,
+        position: {
+          lat: 54.346514,
+          lng: 18.646837
+        }
+      },
+      about: 'krótki opis',
+      image: 'jakies zdjecie',
+      WHstatus: false,
+      ID: 'xxxx'
 
-  },
-  {
-    nazwa: 'Kościół Świętej Trójcy',
-    typ: 'kościół',
-    adres:{
-      street: 'Szymborskiej',
-      number: 4,
-      position: {
-        lat: 54.346514,
-        lng: 18.646837
-      }
     },
-    about: 'krótki opis',
-    image: 'jakies zdjecie',
-    WHstatus: false,
-    ID: 'xxxx'
+    {
+      nazwa: 'Pomnik Poległych Stoczniowców 1970',
+      typ: 'pomnik',
+      adres: {
+        street: 'Reja',
+        number: 4,
+        position: {
+          lat: 54.360548,
+          lng: 18.649052
+        }
+      },
+      about: 'krótki opis',
+      image: 'jakies zdjecie',
+      WHstatus: false,
+      ID: 'xxxx'
 
-  },
-  {
-    nazwa: 'Pomnik Poległych Stoczniowców 1970',
-    typ: 'pomnik',
-    adres:{
-      street: 'Reja',
-      number: 4,
-      position: {
-        lat: 54.360548,
-        lng: 18.649052
-      }
     },
-    about: 'krótki opis',
-    image: 'jakies zdjecie',
-    WHstatus: false,
-    ID: 'xxxx'
+    {
+      nazwa: 'Europejskie Centrum Solidarności',
+      typ: 'muzeum',
+      adres: {
+        street: 'Mickiewicza',
+        number: 4,
+        position: {
+          lat: 54.361258,
+          lng: 18.649480
+        }
+      },
+      about: 'krótki opis',
+      image: 'jakies zdjecie',
+      WHstatus: false,
+      ID: 'xxxx'
 
-  },
-  {
-    nazwa: 'Europejskie Centrum Solidarności',
-    typ: 'muzeum',
-    adres:{
-      street: 'Mickiewicza',
-      number: 4,
-      position: {
-        lat: 54.361258,
-        lng: 18.649480
-      }
     },
-    about: 'krótki opis',
-    image: 'jakies zdjecie',
-    WHstatus: false,
-    ID: 'xxxx'
+    {
+      nazwa: 'Kościół św. Katarzyny',
+      typ: 'kościół',
+      adres: {
+        street: 'Reja',
+        number: 4,
+        position: {
+          lat: 54.354138,
+          lng: 18.651482
+        }
+      },
+      about: 'krótki opis',
+      image: 'jakies zdjecie',
+      WHstatus: true,
+      ID: 'xxxx'
 
-  },
-  {
-    nazwa: 'Kościół św. Katarzyny',
-    typ: 'kościół',
-    adres:{
-      street: 'Reja',
-      number: 4,
-      position: {
-        lat: 54.354138,
-        lng: 18.651482
-      }
     },
-    about: 'krótki opis',
-    image: 'jakies zdjecie',
-    WHstatus: true,
-    ID: 'xxxx'
+    {
+      nazwa: 'Pomnik Marszałka Józefa Piłsudskiego',
+      typ: 'pomnik',
+      adres: {
+        street: 'Reja',
+        number: 4,
+        position: {
+          lat: 54.386421,
+          lng: 18.591431
+        }
+      },
+      about: 'krótki opis',
+      image: 'jakies zdjecie',
+      WHstatus: false,
+      ID: 'xxxx'
 
-  },
-  {
-    nazwa: 'Pomnik Marszałka Józefa Piłsudskiego',
-    typ: 'pomnik',
-    adres:{
-      street: 'Reja',
-      number: 4,
-      position: {
-        lat: 54.386421,
-        lng: 18.591431
-      }
     },
-    about: 'krótki opis',
-    image: 'jakies zdjecie',
-    WHstatus: false,
-    ID: 'xxxx'
+    {
+      nazwa: 'Narodowe Muzeum Morskie w Gdańsku',
+      typ: 'muzeum',
+      adres: {
+        street: 'Reja',
+        number: 4,
+        position: {
+          lat: 54.350901,
+          lng: 18.659051
+        }
+      },
+      about: 'krótki opis',
+      image: 'jakies zdjecie',
+      WHstatus: true,
+      ID: 'xxxx'
 
-  },
-  {
-    nazwa: 'Narodowe Muzeum Morskie w Gdańsku',
-    typ: 'muzeum',
-    adres:{
-      street: 'Reja',
-      number: 4,
-      position: {
-        lat: 54.350901,
-        lng: 18.659051
-      }
     },
-    about: 'krótki opis',
-    image: 'jakies zdjecie',
-    WHstatus: true,
-    ID: 'xxxx'
+    {
+      nazwa: 'Klasztor Ojców Dominikanów',
+      typ: 'kościół',
+      adres: {
+        street: 'Mickiewicza',
+        number: 2,
+        position: {
+          lat: 54.352126,
+          lng: 18.651477
+        }
+      },
+      about: 'krótki opis',
+      image: 'jakies zdjecie',
+      WHstatus: false,
+      ID: 'xxxx'
 
-  },
-  {
-    nazwa: 'Klasztor Ojców Dominikanów',
-    typ: 'kościół',
-    adres:{
-      street: 'Mickiewicza',
-      number: 2,
-      position: {
-        lat: 54.352126,
-        lng: 18.651477
-      }
-    },
-    about: 'krótki opis',
-    image: 'jakies zdjecie',
-    WHstatus: false,
-    ID: 'xxxx'
-
-  }
-];
-
-function initMap() {
-  var mariacka = {lat: 54.349950, lng: 18.652322};
-
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: mariacka,
-    zoom: 14
-  });
-
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  callback(monuments);
-}
-
-function callback(monuments) {
-      for (var i = 0; i < monuments.length; i++) {
-      createMarker(monuments[i]);
     }
+  ];
 
-}
+  angular.module('Workshop', ['uiGmapgoogle-maps'])
+      .controller('mainController', mainController);
 
-function createMarker(place) {
-  var placeLoc = place.adres.position;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.adres.position
-  });
+  function mainController($scope) {
+    $scope.map = {
+      center: {
+        latitude: 40.1451,
+        longitude: -99.6680
+      },
+      zoom: 4,
+      bounds: {}
+    };
+    $scope.options = {
+      scrollwheel: true
+    };
+    var createRandomMarker = function(i, bounds, idKey) {
+      var lat_min = bounds.southwest.latitude,
+          lat_range = bounds.northeast.latitude - lat_min,
+          lng_min = bounds.southwest.longitude,
+          lng_range = bounds.northeast.longitude - lng_min;
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
-// map with markers - end
+      if (idKey == null) {
+        idKey = "id";
+      }
+
+      var latitude = lat_min + (Math.random() * lat_range);
+      var longitude = lng_min + (Math.random() * lng_range);
+      var ret = {
+        latitude: latitude,
+        longitude: longitude,
+        title: 'm' + i
+      };
+      ret[idKey] = i;
+      return ret;
+    };
+    $scope.randomMarkers = [];
+    // Get the bounds from the map once it's loaded
+    $scope.$watch(function() {
+      return $scope.map.bounds;
+    }, function(nv, ov) {
+      // Only need to regenerate once
+      if (!ov.southwest && nv.southwest) {
+        var markers = [];
+        for (var i = 0; i < 50; i++) {
+          markers.push(createRandomMarker(i, $scope.map.bounds))
+        }
+        $scope.randomMarkers = markers;
+      }
+    }, true);
+  }
+
+})();
