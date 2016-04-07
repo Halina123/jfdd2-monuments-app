9a0143cb1c39d6cae6cfc15f40dfea94d392d0fa
KubaK <loirino12345@gmail.com>
2016-04-07 15:53:20 +0200

    Google Sign Out

--- scripts/loginGoogle.js
+++ scripts/loginGoogle.js
@@ -1,7 +1,8 @@
-
+//
 //function onSuccess(googleUser) {
 //  console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-//  window.location = "http://localhost:63342/jfdd2-monuments-app/index.html"
+//  //location.reload();
+//  //window.location = "http://localhost:63342/jfdd2-monuments-app/index.html"
 //}
 //function onFailure(error) {
 //  console.log(error);
@@ -16,6 +17,16 @@
 //    'theme': 'dark',
 //    'onsuccess': onSuccess,
 //    'onfailure': onFailure
+//  });
+//}
+//
+//
+//
+//function signOut() {
+//  var auth2 = gapi.auth2.getAuthInstance();
+//  auth2.signOut().then(function () {
+//    console.log('User signed out.');
+//    //window.location = "http://localhost:63342/jfdd2-monuments-app/login.html"
 //  });
 //}
 
@@ -78,56 +89,58 @@
 
 
 
-//(function() {
-//  var po = document.createElement('script');
-//  po.type = 'text/javascript'; po.async = true;
-//  po.src = 'https://plus.google.com/js/client:plusone.js';
-//  var s = document.getElementsByTagName('script')[0];
-//  s.parentNode.insertBefore(po, s);
-//})();
-//
-//var gpclass = (function(){
-//
-//  //Defining Class Variables here
-//  var response = undefined;
-//  return {
-//    //Class functions / Objects
-//
-//    mycoddeSignIn:function(response){
-//      // The user is signed in
-//      if (response['access_token']) {
-//
-//        //Get User Info from Google Plus API
-//        gapi.client.load('plus','v1',this.getUserInformation);
-//        window.location = "http://localhost:63342/jfdd2-monuments-app/index.html"
-//      } else if (response['error']) {
-//        // There was an error, which means the user is not signed in.
-//        //alert('There was an error: ' + authResult['error']);
-//      }
-//    },
-//
-//    getUserInformation: function(){
-//      var request = gapi.client.plus.people.get( {'userId' : 'me'} );
-//      request.execute( function(profile) {
-//        var email = profile['emails'].filter(function(v) {
-//          return v.type === 'account'; // Filter out the primary email
-//        })[0].value;
-//        var fName = profile.displayName;
-//        $("#inputFullname").val(fName);
-//        $("#inputEmail").val(email);
-//      });
-//    }
-//
-//  }; //End of Return
-//})();
-//
-//function mycoddeSignIn(gpSignInResponse){
-//  gpclass.mycoddeSignIn(gpSignInResponse);
-//}
-//function signOut() {
-//  var auth2 = gapi.auth2.getAuthInstance();
-//  auth2.signOut().then(function () {
-//    console.log('User signed out.');
-//  });
-//}
+(function() {
+  var po = document.createElement('script');
+  po.type = 'text/javascript'; po.async = true;
+  po.src = 'https://plus.google.com/js/client:plusone.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(po, s);
+})();
+
+
+
+var gpclass = (function(){
+
+  //Defining Class Variables here
+  var response = undefined;
+  return {
+    //Class functions / Objects
+
+    mycoddeSignIn:function(response){
+      // The user is signed in
+      if (response['access_token']) {
+
+        //Get User Info from Google Plus API
+        gapi.client.load('plus','v1',this.getUserInformation);
+        window.location = "http://localhost:63342/jfdd2-monuments-app/index.html"
+      } else if (response['error']) {
+        // There was an error, which means the user is not signed in.
+        //alert('There was an error: ' + authResult['error']);
+      }
+    },
+
+    getUserInformation: function(){
+      var request = gapi.client.plus.people.get( {'userId' : 'me'} );
+      request.execute( function(profile) {
+        var email = profile['emails'].filter(function(v) {
+          return v.type === 'account'; // Filter out the primary email
+        })[0].value;
+        var fName = profile.displayName;
+        $("#inputFullname").val(fName);
+        $("#inputEmail").val(email);
+      });
+    }
+
+  }; //End of Return
+})();
+
+function mycoddeSignIn(gpSignInResponse){
+  gpclass.mycoddeSignIn(gpSignInResponse);
+}
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
 
