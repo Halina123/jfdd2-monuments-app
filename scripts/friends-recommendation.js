87989493898a57d29b0fd685f2c2b59de5c742dd
szyszkowski <szysza0x@gmail.com>
2016-04-20 23:59:20 +0200

    Filtering, Recommended, Favourites from Api

--- scripts/friends-recommendation.js
+++ scripts/friends-recommendation.js
@@ -8,66 +8,65 @@ var $inputObiekt = $('#obiektPolec');
 var $inputLogin = $('#loginPolec');
 
 function store() {
-        $.ajax({
-            type: 'get',
-            url: URL + '/recommendations?filter[where][appId]=monuments&filter[where][objectId]=' + object.name +
-            '&filter[where][receiverId]=' + $inputEmail.val() + '&filter[where][senderId]=' + login,
-            dataType: 'json',
-            success: function (result) {
-                if (result.length === 0) {
-                     toAddRec = {
-                        "appId": "monuments",
-                        "senderId": login,
-                        "receiverId": $inputEmail.val(),
-                        "objectType": "REC",
-                        "objectId": object.name,
-                        "id": 0
-                    };
-                    $.ajax({
-                        type: 'POST',
-                        url: URL + '/recommendations',
-                        dataType: 'json',
-                        data: toAddRec,
-                        success: function () {
-                            checkOnLogin();
-                        }
-                    });
-                }
+    $.ajax({
+        type: 'get',
+        url: URL + '/recommendations?filter[where][appId]=monuments&filter[where][objectId]=' + object.name +
+        '&filter[where][receiverId]=' + $inputEmail.val() + '&filter[where][senderId]=' + login,
+        dataType: 'json',
+        success: function (result) {
+            if (result.length === 0) {
+                toAddRec = {
+                    "appId": "monuments",
+                    "senderId": login,
+                    "receiverId": $inputEmail.val(),
+                    "objectType": "REC",
+                    "objectId": object.name,
+                    "id": 0
+                };
+                $.ajax({
+                    type: 'POST',
+                    url: URL + '/recommendations',
+                    dataType: 'json',
+                    data: toAddRec,
+                    success: function () {
+                        checkOnLogin();
+                    }
+                });
             }
-        });
+        }
+    });
 
 
-  $form.css({'display': 'none'});
-  var $alert = $('<p>').text('Dziękuję za Twoje polecenie zabytku');
-  $('#alert').append($alert);
+    $form.css({'display': 'none'});
+    var $alert = $('<p>').text('Dziękuję za Twoje polecenie zabytku');
+    $('#alert').append($alert);
 }
-$(document).ready(function() {
-$form.submit(function (item) {
-    if ($.trim($inputLogin.val()) === "" || $.trim($inputEmail.val()) === ""){
-      alert('Wypełnij wszystkie pola , aby polecenie zabytku było możliwe');
-      return false;
-    }
-    store();
-  });
+$(document).ready(function () {
+    $form.submit(function (item) {
+        if ($.trim($inputLogin.val()) === "" || $.trim($inputEmail.val()) === "") {
+            alert('Wypełnij wszystkie pola , aby polecenie zabytku było możliwe');
+            return false;
+        }
+        store();
+    });
 
 });
 
 
-function numberOfRec () {
-    $('#numberOfRecommendation').html(recommanded.length);
+function numberOfRec() {
+    $('#numberOfRecommendation').html(recommended.length);
     $('#numberOfFavourites').html(favourites.length);
 
 }
 
 
-
-function wyczyscForm(){
+function wyczyscForm() {
     $form[0].reset();
     $form.removeAttr('style');
     $('#alert').html('');
 }
 
-$('#btnPolec').click(function(){
+$('#btnPolec').click(function () {
     alert('tq');
     //$('#modalPolec').modal('show');
 });
